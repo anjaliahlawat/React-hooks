502b55789b2bd87ab6a0c5b81bbb7b2d91656ecf
Anjali <anjali@primesellerhub>
2020-02-15 18:03:40 +0530

    hoc

--- src/HOC/withCounter.js
+++ src/HOC/withCounter.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-const UpdatedComponent = OriginalComponent => {
-    class NewComponent extends Component {
+const withCounter = WrappedComponent => {
+    class WithCounter extends Component {
         constructor(props){
             super(props)
 
@@ -16,12 +16,12 @@ const UpdatedComponent = OriginalComponent => {
         }
         render() {
             return (
-                <OriginalComponent count={this.state.count} 
+                <WrappedComponent count={this.state.count} 
                 incrementCount={this.incrementCount} />
             );
         }
     }
-    return NewComponent
+    return WithCounter
 }
 
-export default UpdatedComponent;
+export default withCounter;
